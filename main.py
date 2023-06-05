371cc974a1a60581ec7c1f6dd9f09ab55077dca7
Wolfofficials <92360356+Wolfofficials@users.noreply.github.com>
2023-06-05 23:09:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import asyncio
 import random
-from telethon import TelegramClient
+from telethon import TelegramClient, events
 from telethon.errors.rpcerrorlist import PeerFloodError
 from telethon.sessions import StringSession
-from telethon.tl.functions.channels import InviteToChannelRequest
-
 from creds import session, api_id, api_hash
+
+replied_chats = set()  # To keep track of already replied chats
 
 # Create a client instance using the string session
 client = TelegramClient(StringSession(session), int(api_id), api_hash)
@@ -24,7 +24,7 @@ with open('texts.txt', 'r') as f:
 # Define the send_message function with a time delay and a random message
 async def send_message_with_delay_to_group(group):
     # Set the delay time in seconds between messages
-    message_delay_time = 270
+    message_delay_time = 5
 
     # Send the random message to the target group
     random_message = random.choice(messages)
@@ -33,14 +33,26 @@ async def send_message_with_delay_to_group(group):
     except PeerFloodError as e:
         print("Waiting for", e.seconds, "seconds due to flood...")
         await asyncio.sleep(e.seconds)
+    except Exception as e:
+        print("Error occurred:", e)
 
     # Wait for the delay time between messages
     await asyncio.sleep(message_delay_time)
 
+# Define the auto_reply function to automatically reply to incoming messages
+async def auto_reply(event):
+    sender = await event.get_sender()
+    if event.is_private and not sender.bot and event.chat_id not in replied_chats:
+        message = event.message.message.lower()
+        reply = '''Hello, watch my vip videos only at 
+        https://t.me/+1BLchlZjMQM5NGNl'''
+        await event.reply(reply)
+        replied_chats.add(event.chat_id)
+
 # Define the main loop that sends messages to all groups indefinitely
 async def main():
     # Set the delay time in seconds between sending messages to groups
-    group_delay_time = 270
+    group_delay_time = 1
     
     while True:
         for group in groups:
@@ -52,9 +64,10 @@ async def main():
         print("Finished sending messages to all groups, starting again...")
         await asyncio.sleep(5)
 
-# Run the main loop until interrupted
-with client:
-    try:
-        client.loop.run_until_complete(main())
-    except KeyboardInterrupt:
-        print("Received interrupt signal, exiting...") 
+# Run the main loop and auto_reply concurrently until interrupted
+try:
+    asyncio.ensure_future(main())
+    client.add_event_handler(auto_reply, events.NewMessage(incoming=True))
+    client.run_until_disconnected()
+except KeyboardInterrupt:
+    print("Received interrupt signal, exiting...")
